4d587b9f1d3b13e6bd2ac4ef9345a92ec31018d6
Felix Limanta <13515065@std.stei.itb.ac.id>
2017-06-01 11:55:48 +0700

    Added CLI View

--- src/Main/java/GitSearch.java
+++ src/Main/java/GitSearch.java
@@ -1,42 +1,12 @@
 package org.felixlimanta.gitsearch;
 
-import java.util.ArrayList;
-import org.felixlimanta.gitsearch.controller.GitHubUserRepositoryGetter;
-import org.felixlimanta.gitsearch.controller.GitHubUserSearcher;
-import org.felixlimanta.gitsearch.model.GitHubRepository;
-import org.felixlimanta.gitsearch.model.GitHubSearchUserUrlGenerator;
-import org.felixlimanta.gitsearch.model.GitHubUser;
+import org.felixlimanta.gitsearch.view.CliView;
 
 /**
  * Created by ASUS on 28/05/17.
  */
 public class GitSearch {
   public static void main(String args[]) throws Exception {
-    GitHubSearchUserUrlGenerator query = new GitHubSearchUserUrlGenerator("holy");
-    query.setSearchIn(1);
-    query.setRepoFilter(true, ">=", 5);
-    query.setFollowerFilter(true, "<=", 10);
-    String url = query.generateUrl();
-
-    GitHubUserSearcher g = new GitHubUserSearcher(url);
-    g.retrieveDataFromGitHub();
-
-    ArrayList<GitHubUser> users = g.getResponse().getItems();
-    for (GitHubUser u: users) {
-      System.out.printf("%d: %s | %s\n", u.getId(), u.getUsername(), u.getHtmlUrl());
-    }
-    System.out.println();
-
-    for (GitHubUser u: users) {
-      GitHubUserRepositoryGetter r = new GitHubUserRepositoryGetter(u);
-      r.retrieveDataFromGitHub();
-      u.setRepos(r.getResponse());
-
-      System.out.printf("%d: %s | %s\n", u.getId(), u.getUsername(), u.getHtmlUrl());
-      for (GitHubRepository rep: r.getResponse()) {
-        System.out.printf("\t%d: %s | %s | %s\n", rep.getId(), rep.getName(), rep.getDescription(), rep.getHtmlUrl());
-      }
-      System.out.printf("%d repos\n\n", u.getRepos().size());
-    }
+    new CliView();
   }
 }
---
+++ src/Main/java/view/CliView.java
@@ -0,0 +1,95 @@
+package org.felixlimanta.gitsearch.view;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import org.felixlimanta.gitsearch.controller.GitHubUserRepositoryGetter;
+import org.felixlimanta.gitsearch.controller.GitHubUserSearcher;
+import org.felixlimanta.gitsearch.model.GitHubRepository;
+import org.felixlimanta.gitsearch.model.GitHubSearchUserUrlGenerator;
+import org.felixlimanta.gitsearch.model.GitHubUser;
+
+/**
+ * Created by ASUS on 01/06/17.
+ */
+public class CliView {
+  private Scanner scan;
+  private String url;
+  private ArrayList<GitHubUser> users;
+
+  public CliView() {
+    scan = new Scanner(System.in);
+    getUserInput();
+    searchUsers();
+    getRepo();
+  }
+
+  public void getUserInput() {
+    System.out.print("Query: ");
+    String queryString = scan.nextLine();
+    GitHubSearchUserUrlGenerator query = new GitHubSearchUserUrlGenerator(queryString);
+    System.out.println();
+
+    System.out.println("0. Search all");
+    System.out.println("1. Search username");
+    System.out.println("2. Search email");
+    System.out.println("3. Search full name");
+    System.out.print(": ");
+    int searchIn = scan.nextInt();
+    query.setSearchIn(searchIn);
+    System.out.println();
+
+    System.out.print("Limit repositories? [y/N] ");
+    char repo = scan.next().charAt(0);
+    if (repo == 'Y' || repo == 'y') {
+      System.out.print("Operator [= / > / < / >= / <=] ");
+      String opr = scan.nextLine();
+      System.out.print("Limit: ");
+      int limit = scan.nextInt();
+      query.setRepoFilter(true, opr, limit);
+    } else {
+      query.setRepoUsed(false);
+    }
+    System.out.println();
+
+    System.out.print("Limit followers? [y/N] ");
+    char follower = scan.next().charAt(0);
+    if (follower == 'Y' || follower == 'y') {
+      System.out.print("Operator [= / > / < / >= / <=] ");
+      String opr = scan.nextLine();
+      System.out.print("Limit: ");
+      int limit = scan.nextInt();
+      query.setFollowerFilter(true, opr, limit);
+    } else {
+      query.setFollowerUsed(false);
+    }
+    System.out.println();
+
+    url = query.generateUrl();
+  }
+
+  public void searchUsers() {
+    GitHubUserSearcher g = new GitHubUserSearcher(url);
+    g.retrieveDataFromGitHub();
+
+    users = g.getResponse().getItems();
+    for (int i = 0; i < users.size(); ++i) {
+      System.out.printf("%d. %s | %s\n", i, users.get(i).getUsername(), users.get(i).getHtmlUrl());
+    }
+    System.out.println();
+  }
+
+  public void getRepo() {
+    System.out.print("User to get repos: ");
+    int i = scan.nextInt();
+
+    GitHubUserRepositoryGetter r = new GitHubUserRepositoryGetter(users.get(i));
+    r.retrieveDataFromGitHub();
+    users.get(i).setRepos(r.getResponse());
+
+    System.out.printf("%d: %s | %s\n", users.get(i).getId(), users.get(i).getUsername(), users.get(i).getHtmlUrl());
+    for (GitHubRepository rep: r.getResponse()) {
+      System.out.printf("\t%d: %s | %s | %s\n", rep.getId(), rep.getName(), rep.getDescription(), rep.getHtmlUrl());
+    }
+    System.out.printf("%d repos\n\n", users.get(i).getRepos().size());
+  }
+}
